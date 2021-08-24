05dd9168165bcb5edb7780e2d70e2d6abf0455d7
ark <67444302+ark-maker-bot@users.noreply.github.com>
2021-08-24 22:40:24 +0100

    added new homepage

---
+++ assets/js/utils.js
@@ -0,0 +1,7 @@
+function secretHEHE(){
+  window.location.href = "./donut/index.html";
+}
+function copyTag(){
+  navigator.clipboard.writeText('.nero#3887');
+  alert('Tag has been added to your clipboard')
+}
